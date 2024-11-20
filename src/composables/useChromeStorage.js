13e5cc9e1315d8f3d00f9c314d130df0ad982951
iron.min <iron.min@kakaocorp.com>
2024-11-21 00:17:16 +0900

    화면 전환시 프롬프트가 초기화 되는 문제 수정

--- src/composables/useChromeStorage.js
+++ src/composables/useChromeStorage.js
@@ -31,21 +31,20 @@ export default function useChromeStorage() {
     }
 
     async function loadPrompts() {
-        return new Promise(resolve => {
-            if (!loaded.value) { // 이미 로드된 경우 실행하지 않음
-                get('prompts', (data) => {
-                    // 불러온 데이터가 배열인지 확인하고, 아니면 빈 배열로 초기화
-                    if (Array.isArray(data.prompts)) {
-                        prompts.value = data.prompts;
-                    } else {
-                        prompts.value = data.prompts ? Object.values(data.prompts) : [];
-                    }
-                    loaded.value = true; // 데이터 로딩 완료 표시
-                });
-            }
-            resolve();
+        if (loaded.value) return; // 이미 로드된 경우 실행하지 않음
+        return new Promise((resolve) => {
+            get('prompts', (data) => {
+                // 불러온 데이터가 배열인지 확인하고, 아니면 빈 배열로 초기화
+                if (Array.isArray(data.prompts)) {
+                    prompts.value = data.prompts;
+                } else {
+                    prompts.value = data.prompts ? Object.values(data.prompts) : [];
+                }
+                loaded.value = true; // 데이터 로딩 완료 표시
+                resolve();
+            });
         });
-    }
+    };
 
     // 컴포넌트에서 호출할 수 있도록 초기화를 유지함
     loadPrompts();

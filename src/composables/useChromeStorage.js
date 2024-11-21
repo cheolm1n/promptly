4afdc3ad6d2a0252282ef505e01e38d57e911190
이종우 (Lee Jongwoo) <jongwoo328@gmail.com>
2024-11-21 18:17:04 +0900

    잘못 작성된 코드 수정 (#7)
    * 잘못 작성된 코드 수정
    
    * loaded 수정 코드 위치 변경
    
    * 잘못 작성된 storage 코드 수정
    
    * 화면 전환시 프롬프트가 초기화 되는 문제 수정
    
    ---------
    
    Co-authored-by: iron.min <iron.min@kakaocorp.com>

--- src/composables/useChromeStorage.js
+++ src/composables/useChromeStorage.js
@@ -1,29 +1,28 @@
 import {ref} from "vue";
 
-const alternativeStorage = {};
-
 export default function useChromeStorage() {
     const prompts = ref([]);
     const loaded = ref(false);
-    const isChromeStorageAvailable = ref(typeof window.chrome !== 'undefined' && window.chrome.storage);
+    const isChromeStorageAvailable = ref(typeof window.chrome !== 'undefined' && window.chrome.storage !== undefined);
 
     function get(key, callback) {
-        if (isChromeStorageAvailable) {
+        if (isChromeStorageAvailable.value) {
             window.chrome.storage.sync.get(key, callback);
         } else {
-            alternativeStorage[key] = JSON.parse(localStorage.getItem(key));
+            const data = {}
+            data[key] = JSON.parse(localStorage.getItem(key));
             if (callback) {
-                callback(alternativeStorage);
+                callback(data);
             }
         }
     }
 
-    function set(key, callback) {
-        if (isChromeStorageAvailable) {
-            window.chrome.storage.sync.set(key, callback);
+    function set(data, callback) {
+        if (isChromeStorageAvailable.value) {
+            window.chrome.storage.sync.set(data, callback);
         } else {
-            for (const key in alternativeStorage) {
-                localStorage.setItem(key, JSON.stringify(alternativeStorage[key]));
+            for (const key in data) {
+                localStorage.setItem(key, JSON.stringify(data[key]));
             }
             if (callback) {
                 callback();
@@ -31,22 +30,21 @@ export default function useChromeStorage() {
         }
     }
 
-    function loadPrompts() {
-        return new Promise(resolve => {
-            if (!loaded.value) { // 이미 로드된 경우 실행하지 않음
-                get('prompts', (data) => {
-                    // 불러온 데이터가 배열인지 확인하고, 아니면 빈 배열로 초기화
-                    if (Array.isArray(data.prompts)) {
-                        prompts.value = data.prompts;
-                    } else {
-                        prompts.value = data.prompts ? Object.values(data.prompts) : [];
-                        loaded.value = true; // 데이터 로딩 완료 표시
-                    }
-                    resolve(prompts);
-                });
-            }
+    async function loadPrompts() {
+        if (loaded.value) return; // 이미 로드된 경우 실행하지 않음
+        return new Promise((resolve) => {
+            get('prompts', (data) => {
+                // 불러온 데이터가 배열인지 확인하고, 아니면 빈 배열로 초기화
+                if (Array.isArray(data.prompts)) {
+                    prompts.value = data.prompts;
+                } else {
+                    prompts.value = data.prompts ? Object.values(data.prompts) : [];
+                }
+                loaded.value = true; // 데이터 로딩 완료 표시
+                resolve();
+            });
         });
-    }
+    };
 
     // 컴포넌트에서 호출할 수 있도록 초기화를 유지함
     loadPrompts();

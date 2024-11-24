08a6bd4912c4245e4a6a55474d08888894e9c9e4
주철민 <cheolm1n@naver.com>
2024-11-25 00:15:38 +0900

    Feature/example 첫 사용자를 위한 샘플 추가 (#10)
    * 첫 사용시에 샘플 프롬프트를 추가
    
    * readme.md 추가 개선 사항 업데이트
    
    * isFirstUse -> hasUsedBefore 로 변수명 변경
    
    ---------
    
    Co-authored-by: iron.min <iron.min@kakaocorp.com>

--- src/composables/useChromeStorage.js
+++ src/composables/useChromeStorage.js
@@ -1,4 +1,4 @@
-import {ref} from "vue";
+import { ref } from "vue";
 
 export default function useChromeStorage() {
     const prompts = ref([]);
@@ -33,15 +33,30 @@ export default function useChromeStorage() {
     async function loadPrompts() {
         if (loaded.value) return; // 이미 로드된 경우 실행하지 않음
         return new Promise((resolve) => {
-            get('prompts', (data) => {
-                // 불러온 데이터가 배열인지 확인하고, 아니면 빈 배열로 초기화
-                if (Array.isArray(data.prompts)) {
-                    prompts.value = data.prompts;
-                } else {
-                    prompts.value = data.prompts ? Object.values(data.prompts) : [];
-                }
-                loaded.value = true; // 데이터 로딩 완료 표시
-                resolve();
+            get('hasUsedBefore', (initData) => {
+                get('prompts', (data) => {
+                    if (!initData.hasUsedBefore && (!data.prompts || data.prompts.length === 0)) {
+                        // 첫 사용이고 저장된 프롬프트가 없을 경우 기본 예제 추가
+                        const defaultPrompts = [
+                            "(샘플) 다음 텍스트를 {언어}로 번역해주세요:\n{텍스트}",
+                            "(샘플) {문제 상황}에서 {선택지1}, {선택지2} 중 하나를 선택하는 데 도움을 주세요. 각 선택의 장단점을 비교해주세요.",
+                            "(샘플) {제품명}의 {타겟 시장}을 위한 효과적인 마케팅 전략을 작성해주세요.\n주요 목표는 {목표}입니다."
+                        ];
+                        prompts.value = defaultPrompts;
+                        set({ prompts: defaultPrompts.map(prompt => prompt), hasUsedBefore: true }, () => {
+                            loaded.value = true; // 데이터 로딩 완료 표시
+                            resolve();
+                        });
+                    } else {
+                        if (Array.isArray(data.prompts)) {
+                            prompts.value = data.prompts;
+                        } else {
+                            prompts.value = data.prompts ? Object.values(data.prompts) : [];
+                        }
+                        loaded.value = true; // 데이터 로딩 완료 표시
+                        resolve();
+                    }
+                });
             });
         });
     };

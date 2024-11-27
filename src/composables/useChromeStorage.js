7999384a8ca85415a09561dedb3b970156210924
iron.min <iron.min@kakaocorp.com>
2024-11-27 14:11:37 +0900

    영문 샘플 추가

--- src/composables/useChromeStorage.js
+++ src/composables/useChromeStorage.js
@@ -1,6 +1,9 @@
 import { ref } from "vue";
+import useI18n from "./useChromeI18n";
 
 export default function useChromeStorage() {
+    const { getLocale } = useI18n();
+
     const prompts = ref([]);
     const loaded = ref(false);
     const isChromeStorageAvailable = ref(typeof window.chrome !== 'undefined' && window.chrome.storage !== undefined);
@@ -35,13 +38,23 @@ export default function useChromeStorage() {
         return new Promise((resolve) => {
             get('hasUsedBefore', (initData) => {
                 get('prompts', (data) => {
-                    if (!initData.hasUsedBefore && (!data.prompts || data.prompts.length === 0)) {
+                    const isFirstUse = !initData.hasUsedBefore && (!data.prompts || data.prompts.length === 0);
+                    if (isFirstUse) {
                         // 첫 사용이고 저장된 프롬프트가 없을 경우 기본 예제 추가
-                        const defaultPrompts = [
-                            "(샘플) 다음 텍스트를 {언어}로 번역해주세요:\n{텍스트}",
-                            "(샘플) {문제 상황}에서 {선택지1}, {선택지2} 중 하나를 선택하는 데 도움을 주세요. 각 선택의 장단점을 비교해주세요.",
-                            "(샘플) {제품명}의 {타겟 시장}을 위한 효과적인 마케팅 전략을 작성해주세요.\n주요 목표는 {목표}입니다."
-                        ];
+                        let defaultPrompts;
+                        if (getLocale() === "ko-KR") {
+                            defaultPrompts = [
+                                "(샘플) 다음 텍스트를 {언어}로 번역해주세요:\n{텍스트}",
+                                "(샘플) {문제 상황}에서 {선택지1}, {선택지2} 중 하나를 선택하는 데 도움을 주세요. 각 선택의 장단점을 비교해주세요.",
+                                "(샘플) {제품명}의 {타겟 시장}을 위한 효과적인 마케팅 전략을 작성해주세요.\n주요 목표는 {목표}입니다."
+                            ];
+                        } else {
+                            defaultPrompts = [
+                                "(Sample) Please translate the following text into {language}:\n{text}",
+                                "(Sample) Please help choose between {option1} and {option2} in the context of {situation}. Compare the pros and cons of each option.",
+                                "(Sample) Write an effective marketing strategy for {product} targeting the {target market}. The main goal is {goal}."
+                            ];
+                        }
                         prompts.value = defaultPrompts;
                         set({ prompts: defaultPrompts.map(prompt => prompt), hasUsedBefore: true }, () => {
                             loaded.value = true; // 데이터 로딩 완료 표시

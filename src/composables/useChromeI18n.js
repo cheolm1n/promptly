41b9efd04c552c526ade30a247ece0b86fb66c0d
주철민 <cheolm1n@naver.com>
2024-11-28 23:35:23 +0900

    Feature/i18n i18n 적용 (#15)
    * i18n 적용을 위한 매니페스트 수정
    
    * i18n을 위한 컴포저블 추가
    
    * 한국어/영어 번역 메시지 추가
    
    * i18n 적용
    
    * readme.md 업데이트
    
    * 어색한 표현 교정
    
    * i18n 컴포저블 이름 변경, getLocale 함수 추가
    
    * 영문 샘플 추가
    
    * 어색한 표현 교정
    
    * 샘플 프롬프트 추가시 언어코드가 ko로 올 때의 조건 추가
    
    * 개발 환경에서는 직접 메시지를 불러오도록 수정
    
    ---------
    
    Co-authored-by: iron.min <iron.min@kakaocorp.com>
    Co-authored-by: JongwooLee <jongwoo328@gmail.com>

---
+++ src/composables/useChromeI18n.js
@@ -0,0 +1,39 @@
+import { ref } from 'vue';
+import en from '../../public/_locales/en/messages.json'
+import ko from '../../public/_locales/ko/messages.json'
+
+export default function useI18n() {
+  const isChromeI18nAvailable = ref(typeof window.chrome !== 'undefined' && window.chrome.i18n !== undefined);
+
+  function getLocaleFile(lang) {
+    switch (lang) {
+      case 'ko':
+      case 'ko-KR':
+        return ko;
+      default:
+        return en;
+    }
+  }
+
+  function getMessage(key) {
+    if (isChromeI18nAvailable.value) {
+      return chrome.i18n.getMessage(key);
+    } else {
+      return getLocaleFile(navigator.language)[key].message
+    }
+  };
+
+  function getLocale() {
+    if (isChromeI18nAvailable) {
+      return chrome.i18n.getUILanguage();
+    } else {
+      // Local 개발 환경에서는 한국어 사용
+      return "ko-KR";
+    }
+  };
+
+  return {
+    getMessage,
+    getLocale,
+  };
+}

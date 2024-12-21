fc0650f9740be136a4a7c607406e40cab53c9869
iron.min <iron.min@kakaocorp.com>
2024-12-21 10:20:01 +0900

    프롬프트 검색과 선택된 프롬프트 해제가 가능하도록 수정

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -9,6 +9,8 @@
           :placeholder="getMessage('selectPromptLabel')"
           style="width: 100%; margin-bottom: 1rem;"
           append-to="self"
+          filter
+          showClear
       />
       <div v-if="variables.length" class="variables-container">
         <div

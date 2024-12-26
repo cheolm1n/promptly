108be24aec7190c41c1f905f9889d5a652e47ca9
JongwooLee <jongwoo328@gmail.com>
2024-12-26 18:55:06 +0900

    제목 추가

--- src/composables/useChromeStorage.js
+++ src/composables/useChromeStorage.js
@@ -1,5 +1,6 @@
 import { ref } from "vue";
 import useI18n from "./useChromeI18n";
+import { nanoid } from "nanoid";
 
 export default function useChromeStorage() {
   const { getLocale } = useI18n();
@@ -17,14 +18,14 @@ export default function useChromeStorage() {
           if (chrome.runtime.lastError) {
             reject(chrome.runtime.lastError);
           } else {
-            resolve(result);
+            resolve(result[key]);
           }
         });
       } else {
         try {
           const data = {};
           data[key] = JSON.parse(localStorage.getItem(key));
-          resolve(data);
+          resolve(data[key]);
         } catch (error) {
           reject(error);
         }
@@ -58,33 +59,51 @@ export default function useChromeStorage() {
   async function loadPrompts() {
     if (loaded.value) return; // 이미 로드된 경우 실행하지 않음
     try {
-      const initData = await get("hasUsedBefore");
-      const data = await get("prompts");
+      const hasUsedBefore = await get("hasUsedBefore");
+      const loadedPrompts = await get("prompts");
       const isFirstUse =
-        !initData.hasUsedBefore && (!data.prompts || data.prompts.length === 0);
+        !hasUsedBefore && (!loadedPrompts || loadedPrompts.length === 0);
       if (isFirstUse) {
         // 첫 사용이고 저장된 프롬프트가 없을 경우 기본 예제 추가
         let defaultPrompts;
         if (getLocale() === "ko" || getLocale() === "ko-KR") {
           defaultPrompts = [
-            "(샘플) 다음 텍스트를 {언어}로 번역해주세요:\n{텍스트}",
-            "(샘플) {문제 상황}에서 {선택지1}, {선택지2} 중 하나를 선택하는 데 도움을 주세요. 각 선택의 장단점을 비교해주세요.",
-            "(샘플) {제품명}의 {타겟 시장}을 위한 효과적인 마케팅 전략을 작성해주세요.\n주요 목표는 {목표}입니다.",
+            {
+              title: "예시 1",
+              text: "(샘플) 다음 텍스트를 {언어}로 번역해주세요:\n{텍스트}",
+            },
+            {
+              title: "예시 2",
+              text: "(샘플) {문제 상황}에서 {선택지1}, {선택지2} 중 하나를 선택하는 데 도움을 주세요. 각 선택의 장단점을 비교해주세요.",
+            },
+            {
+              title: "예시 3",
+              text: "(샘플) {제품명}의 {타겟 시장}을 위한 효과적인 마케팅 전략을 작성해주세요.\n주요 목표는 {목표}입니다.",
+            },
           ];
         } else {
           defaultPrompts = [
-            "(Sample) Please translate the following text into {language}:\n{text}",
-            "(Sample) Please help choose between {option1} and {option2} in the context of {situation}. Compare the pros and cons of each option.",
-            "(Sample) Write an effective marketing strategy for {product} targeting the {target market}. The main goal is {goal}.",
+            {
+              title: "Sample 1",
+              text: "(Sample) Please translate the following text into {language}:\n{text}",
+            },
+            {
+              title: "Sample 2",
+              text: "(Sample) Please help choose between {option1} and {option2} in the context of {situation}. Compare the pros and cons of each option.",
+            },
+            {
+              title: "Sample 3",
+              text: "(Sample) Write an effective marketing strategy for {product} targeting the {target market}. The main goal is {goal}.",
+            },
           ];
         }
         prompts.value = defaultPrompts;
         await set({ prompts: defaultPrompts, hasUsedBefore: true });
       } else {
-        if (Array.isArray(data.prompts)) {
-          prompts.value = data.prompts;
+        if (Array.isArray(loadedPrompts)) {
+          prompts.value = loadedPrompts;
         } else {
-          prompts.value = data.prompts ? Object.values(data.prompts) : [];
+          prompts.value = loadedPrompts ? Object.values(loadedPrompts) : [];
         }
       }
       loaded.value = true; // 데이터 로딩 완료 표시
